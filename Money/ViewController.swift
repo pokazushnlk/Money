56a701d5d26a898708615520c91f4139a8892441
Yaroslav Shestakov <shestakov.yaroslav@gmail.com>
2023-02-01 02:55:40 +0300

    Initial Commit

---
+++ Money/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money
+//
+//  Created by Ярослав on 01.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
