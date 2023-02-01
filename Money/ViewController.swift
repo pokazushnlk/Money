17d702900864caf55d5cf1211f356779b95f5984
Yaroslav Shestakov <shestakov.yaroslav@gmail.com>
2023-02-01 23:49:44 +0300

    Start programming my money calculator for salary. In first, i made a keyboard, simple design of app and a simple logic of work. Thank's for attention

--- Money/ViewController.swift
+++ Money/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    var stillTyping = false
+    @IBOutlet var ButtonKeyboard: [UIButton]! {
+        didSet {
+            for button in ButtonKeyboard {
+                button.layer.cornerRadius = 11
+            }
+        }
+    }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func pressNumber(_ sender: UIButton) {
+        let number = sender.currentTitle!
+        if stillTyping == true {
+            if displayLabel.text!.count < 10 {
+                
+                displayLabel.text = displayLabel.text! + number
+                
+                
+            }
+        } else {
+            displayLabel.text! = number
+            stillTyping = true
+        }
+    }
 }
 
